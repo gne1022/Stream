c9e78ab1ebaae379b08f1ca7ee81728f1c1012d3
gne1022 <142654086+gne1022@users.noreply.github.com>
2023-09-05 16:28:31 +0900

    test

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
-view = [200,350,400]
-view
+import streamlit as st
+
+def main():
+    st.title("Somac")
+    menu = ["home","login"]
+    choice = st.sidebar.selectbox("menu",menu)
+
+    if choice == "home":
+        st.subheader("home")
+
+    elif choice == "login":
+        st.subheader("login section")
+
+if __name__ == '__main__':
+    main()
